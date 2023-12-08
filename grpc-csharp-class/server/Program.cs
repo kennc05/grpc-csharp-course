348d8d77509308dc6808127203da1f9f058c340c
Kenneth Cajigas <kenneth.cajigas@tombola.com>
2023-12-08 16:27:45 +0000

    Add GreetServer BoilerPlate code

--- grpc-csharp-class/server/Program.cs
+++ grpc-csharp-class/server/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grpc.Core;
 
 namespace server
 {
     internal class Program
     {
+        const int PORT = 50051;
+
         static void Main(string[] args)
         {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", PORT, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine($"Server listening on port {PORT}");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Server failed to start: {e.Message}");
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
         }
     }
 }
